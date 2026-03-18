6f20f3c00c09c030b7eb68ac36fbf109d8e24bff
Khriji Hazem <hazemkhriji0@gmail.com>
2026-03-18 12:31:38 +0100

    Add solution for A - Segment with Small Sum (trial 1)

---
+++ codeforces/a-segment-with-small-sum_trial1.py
@@ -0,0 +1,13 @@
+n,s=map(int,input().split())
+a=list(map(int,input().split()))
+
+l=0
+curr=0
+res=0
+for r in range(n):
+    curr+=a[r]
+    while curr>s:
+        curr-=a[l]
+        l+=1
+    res=max(res,r-l+1)
+print(res)
